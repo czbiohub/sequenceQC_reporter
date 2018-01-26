103d181e9b2bb6f4cd5be4fcb80aa75492052528
foadgreen <foadgreen@gmail.com>
2018-01-26 13:14:14 -0800

    Cleaned up usage, function inputs/returns, still needs housecleaning.

--- sequenceQC_reporter_functions.R
+++ sequenceQC_reporter_functions.R
@@ -1,22 +1,4 @@
-#Load projects into environment. This function lists the projects in the directory with S3-synced files called by the bash script.
-
-#Get project names
-load_projects = function(PROJECT_DIR){
-  
-  directory_names = list.dirs(PROJECT_DIR, recursive = FALSE)
-  
-  projects = {}
-  for(i in directory_names){
-    projects[[i]]= strsplit(i,"runs/")[[1]][2]
-  }
-  
-  return(list(directory_names, projects))
-}
-
-
-
-#Function to generate alphanumeric index for 384-well plates. This function can be used alone or within subsequent functions. If used alone, input is NUMBER_OF_PLATES. 
-
+#Generate alphanumeric index of 384-well plate format. This function can be used alone or called within other functions. Input is `NUMBER_OF_PLATES`. 
 generate_wells = function(NUMBER_OF_PLATES){
   w_i = {}
   well_index = {}
@@ -31,67 +13,70 @@ generate_wells = function(NUMBER_OF_PLATES){
   }
   
   return(unlist(well_index))
+  
 }
 
-
-
-#Setup your samplesheet for data generation and plotting. This function can be called separately for csv-only (data) and png-only (plot) generation. 
-
-setup_samplesheet = function(PROJECT){
+#Load samplesheet for data generation and plotting. This function can be called separately for csv-only (data) and png-only (plot) generation. 
+loadSamplesheet = function(projectDir){
   
-  setup_samplesheet_list = {}
-  for(i in 1:length(PROJECT[1][1][[1]])){
+  dirs = list.dirs(projectDir, recursive = FALSE)
+  
+  runID = list(strsplit(projectDir,"runs/")[[1]][2])
+  for(i in 1:length(projectDir)){
     
-    path = paste0(PROJECT[[1]][i],"/", PROJECT[[2]][i])
-    dir.create(paste0(path, "/output/"), showWarnings = FALSE)
+    temp = list.files(dirs[2], full.names = TRUE, recursive = TRUE)
     
-    samplesheet = read_csv(paste0(paste0(PROJECT[[1]][i], "/sample-sheets/", PROJECT[[2]][i],".csv")), col_names = TRUE, skip = 1)
+    samplesheet = read_csv(temp, col_names = TRUE, col_types = cols(), skip = 1)
     
     NUMBER_OF_PLATES = nrow(samplesheet)/384
     
     samplesheet$well_index = generate_wells(NUMBER_OF_PLATES)[1:nrow(samplesheet)]
     samplesheet$row = as.factor(samplesheet$well_index %>% str_extract(pattern = "^[A-Z]{1}"))
     samplesheet$col = as.factor(samplesheet$well_index %>% str_extract(pattern = "[0-9]{1,2}"))
-    samplesheet$Sample_Project = PROJECT[[2]][i]
+    #samplesheet$Sample_Project = as.vector(runID[i])
     samplesheet$Description = rep(1:NUMBER_OF_PLATES, each=384)
     
     if(is.integer(samplesheet$`Sample_ID`) == TRUE){
       samplesheet$`Sample_ID` = as.character(samplesheet$`Sample_ID`)
     }
-    
-    setup_samplesheet_list[[i]] = list(path, samplesheet)
+    print(paste0("Samplesheet loaded for sequencing run: ", runID))
   }
   
-  return(setup_samplesheet_list)
+  return(samplesheet)
   
 }
 
-
-
-
 #Generate your FASTQ stats - this function scrapes Illumina bcl2fastq html report files and sorts data like "passed-filter" clusters, samples, and barcodes.
-
-get_BCL2FASTQ_csv = function(PROJECT_INFO){
+loadLaneBarcode = function(projectDir){
   
-  fastq_report_list = {}
+  dirs = list.dirs(projectDir, recursive = FALSE)
   
-  for(i in 1:length(PROJECT_INFO)){
+  runID = {}
+  for(i in projectDir){
     
-    filenames = paste0(strsplit(PROJECT_INFO[[i]][1][[1]], "/[a-zA-Z0-9_-]{1,}$"),"/reports")
+    runID[[i]]= strsplit(i,"runs/")[[1]][2]
     
-    filenames = list.files(filenames, pattern = "laneBarcode.html", full.names = TRUE, recursive = TRUE)
+  }
+  
+  
+  laneBarcodeList = {}
+  for(i in 1:length(projectDir)){
     
-    for(j in filenames){
+    barcodeFile = list.files(dirs[1], pattern = "laneBarcode.html", full.names = TRUE, recursive = TRUE)
+    
+    for(j in barcodeFile){
       
-      clusterList = {}
+      
       sampleList = {}
       barcodeList = {}
+      clusterList = {}
       
       temp = read_lines(j, skip = 42)
       
       sampleList[[j]] = temp[seq(2, length(temp), 14)]
-      clusterList[[j]] = temp[seq(4,length(temp),14)]
       barcodeList[[j]] = temp[seq(3, length(temp), 14)]
+      clusterList[[j]] = temp[seq(4,length(temp),14)]
+      
       
     }
     
@@ -99,7 +84,7 @@ get_BCL2FASTQ_csv = function(PROJECT_INFO){
     clusters = {}
     barcodes = {}
     
-    for(j in filenames){
+    for(j in barcodeFile){
       
       samples[[j]] = unlist(sampleList[[j]])
       clusters[[j]] = unlist(clusterList[[j]])
@@ -107,173 +92,146 @@ get_BCL2FASTQ_csv = function(PROJECT_INFO){
       
     }
     
-    fastq_report = list(unlist(samples), unlist(clusters), unlist(barcodes))
+    laneBarcodeReport = list(unlist(samples), unlist(clusters), unlist(barcodes))
     
-    for (x in 1:length(fastq_report)){fastq_report[[x]] = str_extract(fastq_report[[x]], "(?<=>)[^<]+")}
+    for (x in 1:length(laneBarcodeReport)){laneBarcodeReport[[x]] = str_extract(laneBarcodeReport[[x]], "(?<=>)[^<]+")}
     
-    fastq_report[[2]] = as.numeric(gsub(",", "", fastq_report[[2]]))
-    fastq_report = data_frame(Sample_ID = fastq_report[[1]], clusters = fastq_report[[2]], barcodes = fastq_report[[3]])
-    fastq_report = left_join(PROJECT_INFO[i][[1]][[2]], fastq_report, by = "Sample_ID", sort = FALSE)
+    laneBarcodeReport[[2]] = as.numeric(gsub(",", "", laneBarcodeReport[[2]]))
+    laneBarcode = data_frame(Sample_ID = laneBarcodeReport[[1]], clusters = laneBarcodeReport[[2]], barcodes = laneBarcodeReport[[3]])
     
-    write_csv(fastq_report, paste0(PROJECT_INFO[[i]][1][[1]],"_bcl2fastq_report.csv"))
-    
-    fastq_report_list[[i]] = list(fastq_report)
+    #laneBarcodeList[[i]] = laneBarcode
+    print(paste0("Demultiplexation report loaded for sequencing run: ", runID))
     
   }
   
-  return(fastq_report_list)
+  return(laneBarcode)
+  
 }
 
-
+#Return key-value pair for yourParameter
+return.parameterKey = function(projectDir, yourParameter){
+  
+  filenames = list.files(projectDir, pattern = "*.out", full.names = TRUE, recursive = TRUE, include.dirs = TRUE)
+  temp = read_lines(filenames[1])
+  allParameters = {}
+  
+  for(i in 1:length(temp)){
+    
+    allParameters[[i]] = strsplit(temp[i], " \\|")[[1]][1]
+    allParameters = gsub("^\\s+","", allParameters)
+  }
+  
+  names(allParameters) = allParameters
+  keys = {}
+  keys$index = match(yourParameter, allParameters)
+  keys$keynames = allParameters[yourParameter]
+  
+  checklist = {}; for(j in yourParameter){checklist[[j]] = j %in% keys$keynames}
+  
+  if(any(checklist == FALSE)){
+    print(paste0("Your parameter ", j, " wasn't found. Stopping program."))
+  } else {
+    for(j in yourParameter){
+      print(paste0("Loading your parameter: ", j))
+    }
+    
+    return(keys)
+  }
+}
 
 #Generate your STAR stats - This input for his function is any single line or multiple lines as a PARAMETER (i.e. "Mapped Reads", "Percent Mapped Reads", "Insert Average Length") from the "log.final.out" file. This file a standard log generated afer successful STAR alignment.
 
-get_STAR_csv = function(PROJECT_INFO, YOUR_PARAMETER){
+loadLogFile = function(projectDir, yourParameter){
   
-  star_report_list = {}
+  filenames = list.files(projectDir, pattern = "*.out", full.names = TRUE, recursive = TRUE, include.dirs = TRUE)
+  runID = list(strsplit(projectDir,"runs/")[[1]][2])
+  for(i in 1:length(filenames)){runID$sampleID[[i]] = strsplit(filenames[i], "logs/|_S")[[1]][2]}
+  runID$parameterKey = return.parameterKey(projectDir, yourParameter)
   
-  for(x in 1:length(PROJECT_INFO)){
-    
-    filenames = paste0(strsplit(PROJECT_INFO[[x]][1][[1]], "/[a-zA-Z0-9_-]{1,}$"),"/star_logs")
-    
-    
-    filenames = list.files(filenames, pattern = "*.out", full.names = TRUE, recursive = TRUE, include.dirs = TRUE)
-    
-    sample_id = {}
-    
-    for(i in filenames){
-      sample = strsplit(i, "./results*")[[1]][1]
-      sample = strsplit(sample, "logs/")[[1]][2]
-      sample = strsplit(sample, "_S")[[1]][1]
-      sample_id[[i]] = strsplit(sample, ".mus*")[[1]][1]
-    }
-    
-    logfinalout = {}
-    logfinalout_parameters = {}
-    logfinalout_data = {}
-    
-    for(j in 1:length(filenames)){
-      
-      if(is.na(j)){
-        
-        print("Skipping missing")
+  for(j in 1:length(runID$parameterKey$index)){
+    currentKey = runID$parameterKey$index[j]
+    fileData = {}
+    for(i in 1:length(filenames)){
+      if(is.na(i)){
+        print("No log files for this run. Skipping.")
         break
-        
       } else {
-        temp = read_lines(filenames[j])
-        for(i in 1:length(YOUR_PARAMETER)){
-          k = YOUR_PARAMETER[i]
-          logfinalout_parameters[[i]] = str_split(temp[k], " \\|")[[1]][1]
-          logfinalout_parameters = gsub("^\\s+","", logfinalout_parameters)
-          logfinalout_data[[i]] = str_split(temp[k], "\t")[[1]][2]
-          logfinalout_data = gsub("%","", logfinalout_data)
-        }
-        logfinalout[[j]] = list(logfinalout_parameters, logfinalout_data)
+        temp = read_lines(filenames[i])
+        fileData[[i]] = strsplit(temp[currentKey], "\t")[[1]][2]
+        fileData = gsub("%","", fileData)
       }
     }
-    
-    secondlevel = {}
-    firstlevel = {}
-    
-    for(i in 1:length(YOUR_PARAMETER)){
-      for(j in 1:length(logfinalout)){secondlevel[[j]] = logfinalout[j][[1]][[2]][i]}
-      firstlevel[[i]] = secondlevel
-    }
-    
-    SAMPLE_ID = unlist(sample_id)
-    star_report = data_frame("Sample_ID" = SAMPLE_ID)
-    star_report[ ,ncol(star_report) + 1:length(YOUR_PARAMETER)] = NA
-    
-    for(i in 1:length(YOUR_PARAMETER)){
-      star_report[i+1] = firstlevel[i]
-      colnames(star_report)[i+1]= logfinalout[[1]][[1]][i]
-    }
-    
-    star_report = left_join(PROJECT_INFO[x][[1]][[2]], star_report, by = "Sample_ID", sort = FALSE)
-    write_csv(star_report, paste0(PROJECT_INFO[[x]][1][[1]],"_star_report.csv"))
-    star_report_list[[x]] = list(star_report)
+    runID$parameterData[[j]] = fileData
   }
   
-  return(star_report_list)
+  log = {} ; for(i in 1:length(runID$parameterData)){log[[i]] = as.numeric(unlist(runID$parameterData[i]))}
+  names(log) = unlist(runID$parameterKey$keynames)
+  log$Sample_ID= unlist(runID$sampleID)
+  df = as_tibble(log[1:length(log)])
+  
+  df = left_join(samplesheet, df, by = "Sample_ID", sort = FALSE)
+  #write_csv(df, paste0(projectDir,"/", runID,"_logfinalout.csv"))
+  return(df)
 }
-
-
 
 
 #This function generates heatmaps (and other plots) from your FASTQ data. This function currently calls the get_FASTQ_csv function, but should also be able to get called separately to accept the csv generated by get_FASTQ_csv.
-
-plot_fastq_output = function(fastqPARAMS, PROJECT){
+heatmap.laneBarcode = function(projectDir, samplesheet, laneBarcode){
+  runID = strsplit(projectDir,"runs/")[[1]][2]
+  laneBarcodeReport = left_join(samplesheet, laneBarcode, by = "Sample_ID", sort = FALSE)
+  write_csv(x = laneBarcodeReport, path = paste0(projectDir,"/", runID,"_laneBarcodeReport.csv"))
   
-  fastqPARAMS_plots = {}
-  heatmap_list = {}
-  grid_list = {}
+  print(paste0("Wrote ", runID, ".csv to ", projectDir,"/"))
+  
+  data = laneBarcodeReport
+  data$clusters = as.numeric(data$clusters)
+  
+  plotTitle = paste0("Lane Barcode Heatmap: ", runID, " Parameter: Unmapped clusters")
+  
+  log2_heatmap = ggplot() +
+    geom_raster(data, 
+                mapping = aes(x = col, y = row, fill = log2(data$clusters))) +
+    scale_x_discrete(limits = c(1:24)) +
+    scale_y_discrete(limits = rev(levels(data$row))) +
+    coord_equal() +
+    scale_fill_viridis(guide_legend(title = "log2")) +
+    theme(strip.text = element_blank(),
+          plot.title = element_text(size = 9, hjust = 0.5),
+          legend.title = element_text(size = 7),
+          legend.text = element_text(size = 7),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank()) +
+    facet_wrap(~data$Description) +
+    ggtitle(plotTitle)
+  ggsave(paste0(projectDir,"/", runID, "_laneBarcode_log2scale_heatmap.png"))
+  print("Saved heatmap for log2-scaled clusters")
+  
+  unscaled_heatmap = ggplot() +
+    geom_raster(data, 
+                mapping = aes(x = col, y = row, fill = data$clusters)) +
+    scale_x_discrete(limits = c(1:24)) +
+    scale_y_discrete(limits = rev(levels(data$row))) +
+    coord_equal() +
+    scale_fill_viridis(guide_legend(title = "unscaled")) +
+    theme(strip.text = element_blank(),
+          plot.title = element_text(size = 9, hjust = 0.5),
+          legend.title = element_text(size = 7),
+          legend.text = element_text(size = 7),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank()) +
+    facet_wrap(~data$Description) +
+    ggtitle(plotTitle)
+  ggsave(paste0(projectDir,"/", runID, "_laneBarcode_unscaled_heatmap.png"))
+  print("Saved heatmap for unscaled clusters")
   
   
-  for(x in 1:length(fastqPARAMS)){
-    print(paste0("project", x))
-    
-    data = fastqPARAMS[x][[1]][[1]]
-    PROJECT_TITLE = data$Sample_Project[[1]]
-    data[[14]] = as.numeric(data[[14]])
-    
-    log2_heatmap = {}
-    heatmap = {}
-    
-    PLOT_TITLE = paste0("BCL2FASTQ OUTPUT for ", PROJECT_TITLE, " PARAMETER: ", "PASSED-FILTER Clusters")
-    
-    log2_heatmap[[x]] = ggplot() +
-      geom_raster(data, 
-                  mapping = aes(x = col, y = row, fill = log2(data$clusters))) +
-      scale_x_discrete(limits = c(1:24)) +
-      scale_y_discrete(limits = rev(levels(data$row))) +
-      coord_equal() +
-      #guides(fill = FALSE) +
-      scale_fill_viridis(guide_legend(title = "log2")) +
-      theme(strip.text = element_blank(),
-            plot.title = element_text(size = 9, hjust = 0.5),
-            legend.title = element_text(size = 7),
-            legend.text = element_text(size = 7),
-            axis.title = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text = element_blank()) +
-      facet_wrap(~data$Description) +
-      ggtitle(PLOT_TITLE)
-    ggsave(paste0(PROJECT[[1]][x],"/",PROJECT[[2]][x], "_", "PASSED-FILTER Clusters","_miseq_bcl2fastqreport_log2heatmap.png"))
-    
-    print(log2_heatmap[[x]])
-    
-    heatmap[[x]] = ggplot() +
-      geom_raster(data, 
-                  mapping = aes(x = col, y = row, fill = data$clusters)) +
-      scale_x_discrete(limits = c(1:24)) +
-      scale_y_discrete(limits = rev(levels(data$row))) +
-      coord_equal() +
-      #guides(fill = FALSE) +
-      scale_fill_viridis(guide_legend(title = "unscaled")) +
-      theme(strip.text = element_blank(),
-            plot.title = element_text(size = 9, hjust = 0.5),
-            legend.title = element_text(size = 7),
-            legend.text = element_text(size = 7),
-            axis.title = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text = element_blank()) +
-      facet_wrap(~data$Description) +
-      ggtitle(PLOT_TITLE)
-    ggsave(paste0(PROJECT[[1]][x],"/",PROJECT[[2]][x], "_", "PASSED-FILTER Clusters","_miseq_bcl2fastqreport_heatmap.png"))
-    
-    print(heatmap[[x]])
-    
-  }
+  heatmaps = list(log2_heatmap, unscaled_heatmap)
   
-  heatmap_list = list(log2_heatmap, heatmap)
-  fastqPARAMS_plots = list(all_my_heatmaps = heatmap_list)
-  
-  return(fastqPARAMS_plots)
+  return(heatmaps)
   
 }
-
-
-
 
 #This function generates heatmaps (and other plots) from your STAR data. It's important to maintain the same YOUR_PARAMETER call. This function currently calls the get_STAR_csv function, but should also be able to get called separately to accept the csv generated by get_STAR_csv.
 
@@ -309,9 +267,9 @@ plot_STAR_output = function(starPARAMS, PROJECT, YOUR_PARAMETER){
         guides(fill = FALSE) +
         scale_fill_viridis(guide_legend(title = paste0("log2 ", parameter_to_plot))) +
         theme(strip.text = element_blank(),
-              plot.title = element_text(size = 7, hjust = 0.5),
-              legend.title = element_text(size = 7),
-              legend.text = element_text(size = 7),
+              plot.title = element_text(size = 5, hjust = 0.5),
+              legend.title = element_text(size = 5),
+              legend.text = element_text(size = 5),
               axis.title = element_blank(),
               axis.ticks = element_blank(),
               axis.text = element_blank()) +
